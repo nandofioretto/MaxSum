b887fda6b46a69564ed1d7139c5e509e03ab6ed0
fioretto <fioretto@umich.edu>
2017-08-03 12:36:50 -0400

    added python scripts

--- scripts/ccg_to_dcop.py
+++ scripts/ccg_to_dcop.py
@@ -1,43 +1,6 @@
 import numpy as np
-import json
+import commons as cm
 import sys, getopt, os
-
-
-## Commons I/O
-def makedir(path):
-    if not path or os.path.exists(path):
-        return []
-    (head, tail) = os.path.split(path)
-    res = makedir(head)
-    if not os.path.exists(path):
-        os.mkdir(path)
-    res += [path]
-    return res
-
-
-def load_json_file(json_file: str) -> object:
-    """ Loads a json file into a dictionary
-    :rtype: dict
-    """
-    with open(json_file) as data_file:
-        data = json.load(data_file)
-    return data
-
-
-def save_json_path_file(path: str, filename: str, data: dict):
-    """ Write a dictionary as a json file """
-    makedir(path)
-    if not path.endswith('/'):
-        path += '/'
-    print('saving Json file: ', filename, ' in path: ', path)
-    with open(path + filename, 'w') as outfile:
-        json.dump(data, outfile, indent=2)
-
-
-def save_json_file(pathfile: str, data: dict):
-    """ Write a dictionary as a json file """
-    path, filename = os.path.split(pathfile)
-    save_json_path_file(path, filename, data)
 
 
 def read_ccg(fname):
@@ -242,4 +205,4 @@ if __name__ == '__main__':
     else:
         dcop = make_dcop(wcsp)
         print('saving dcop: agents=', len(dcop['agents']), ' variables=', len(dcop['variables']), ' constraints=', len(dcop['constraints']))
-        save_json_file(outfile, dcop)
+        cm.save_json_file(outfile, dcop)
